930f0189ded50e798ac8d99b011734b70003d694
Jack <jk19011999@gmail.com>
2023-07-28 21:34:31 +0530

    Created basic structure

---
+++ src/test/java/com/thejackfolio/microservices/professionapi/ProfessionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejackfolio.microservices.professionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
