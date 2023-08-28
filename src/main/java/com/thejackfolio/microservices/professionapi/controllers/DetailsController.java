4002a8984bf8a4cda818b98156b859b2e56ced38
Jack <jk19011999@gmail.com>
2023-08-28 14:20:43 +0530

    Added necessary files

--- src/main/java/com/thejackfolio/microservices/professionapi/controllers/DetailsController.java
+++ src/main/java/com/thejackfolio/microservices/professionapi/controllers/DetailsController.java
@@ -5,7 +5,12 @@ import com.thejackfolio.microservices.professionapi.exceptions.MapperException;
 import com.thejackfolio.microservices.professionapi.exceptions.ValidationException;
 import com.thejackfolio.microservices.professionapi.models.Details;
 import com.thejackfolio.microservices.professionapi.services.DetailsService;
+import com.thejackfolio.microservices.professionapi.utilities.StringConstants;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +21,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/details")
 public class DetailsController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DetailsController.class);
+    private boolean isRetryEnabled = false;
+
     @Autowired
     private DetailsService service;
 
+    @Operation(
+            summary = "Save details",
+            description = "Save details and gives the same details response with a message which defines whether the request is successful or not."
+    )
     @PostMapping("/save-details")
+    @Retry(name = "save-details-db-retry", fallbackMethod = "saveDetailsDBRetry")
     public ResponseEntity<Details> saveDetails(@RequestBody Details details){
         Details response = null;
         try{
+            if(isRetryEnabled){
+                LOGGER.info(StringConstants.RETRY_MESSAGE);
+            }
+            if(!isRetryEnabled){
+                isRetryEnabled = true;
+            }
             response = service.saveDetails(details);
         } catch (ValidationException | MapperException | DataBaseOperationException exception){
             if(details == null){
@@ -31,19 +50,48 @@ public class DetailsController {
             details.setResponseMessage(exception.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(details);
         }
+        isRetryEnabled = false;
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    public ResponseEntity<Details> saveDetailsDBRetry(Details details, Exception exception){
+        isRetryEnabled = false;
+        LOGGER.info(StringConstants.FALLBACK_MESSAGE, exception);
+        Details detailsResponse = new Details();
+        detailsResponse.setResponseMessage(StringConstants.FALLBACK_MESSAGE);
+        return ResponseEntity.status(HttpStatus.OK).body(detailsResponse);
+    }
+
+    @Operation(
+            summary = "Get details",
+            description = "It gives the details as response with a message which defines whether the request is successful or not."
+    )
     @GetMapping("/get-details")
+    @Retry(name = "get-details-db-retry", fallbackMethod = "getDetailsDBRetry")
     public ResponseEntity<Details> getDetails(){
         Details response = null;
         try{
+            if(isRetryEnabled){
+                LOGGER.info(StringConstants.RETRY_MESSAGE);
+            }
+            if(!isRetryEnabled){
+                isRetryEnabled = true;
+            }
             response = service.getDetails();
         } catch (ValidationException | MapperException | DataBaseOperationException exception){
             response = new Details();
             response.setResponseMessage(exception.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
+        isRetryEnabled = false;
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    public ResponseEntity<Details> getDetailsDBRetry(Exception exception){
+        isRetryEnabled = false;
+        LOGGER.info(StringConstants.FALLBACK_MESSAGE, exception);
+        Details detailsResponse = new Details();
+        detailsResponse.setResponseMessage(StringConstants.FALLBACK_MESSAGE);
+        return ResponseEntity.status(HttpStatus.OK).body(detailsResponse);
     }
 }
--- src/main/java/com/thejackfolio/microservices/professionapi/controllers/SkillsController.java
+++ src/main/java/com/thejackfolio/microservices/professionapi/controllers/SkillsController.java
@@ -3,9 +3,15 @@ package com.thejackfolio.microservices.professionapi.controllers;
 import com.thejackfolio.microservices.professionapi.exceptions.DataBaseOperationException;
 import com.thejackfolio.microservices.professionapi.exceptions.MapperException;
 import com.thejackfolio.microservices.professionapi.exceptions.ValidationException;
+import com.thejackfolio.microservices.professionapi.models.Details;
 import com.thejackfolio.microservices.professionapi.models.Skill;
 import com.thejackfolio.microservices.professionapi.services.SkillsService;
+import com.thejackfolio.microservices.professionapi.utilities.StringConstants;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +22,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/skills")
 public class SkillsController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SkillsController.class);
+    private boolean isRetryEnabled = false;
+
     @Autowired
     private SkillsService service;
 
+    @Operation(
+            summary = "Save skills",
+            description = "Save skills and gives the same skills response with a message which defines whether the request is successful or not."
+    )
     @PostMapping("/save-skills")
+    @Retry(name = "save-skills-db-retry", fallbackMethod = "saveSkillsDBRetry")
     public ResponseEntity<Skill> saveSkills(@RequestBody Skill skill){
         Skill response = null;
         try{
@@ -34,7 +48,20 @@ public class SkillsController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    public ResponseEntity<Skill> saveSkillsDBRetry(Skill skill, Exception exception){
+        isRetryEnabled = false;
+        LOGGER.info(StringConstants.FALLBACK_MESSAGE, exception);
+        Skill skillResponse = new Skill();
+        skillResponse.setMessage(StringConstants.FALLBACK_MESSAGE);
+        return ResponseEntity.status(HttpStatus.OK).body(skillResponse);
+    }
+
+    @Operation(
+            summary = "Get skills",
+            description = "Get skills with a message which defines whether the request is successful or not."
+    )
     @GetMapping("/get-skills")
+    @Retry(name = "get-skills-db-retry", fallbackMethod = "getSkillsDBRetry")
     public ResponseEntity<Skill> getSkills(){
         Skill response = null;
         try{
@@ -45,5 +72,13 @@ public class SkillsController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    public ResponseEntity<Skill> getSkillsDBRetry(Exception exception){
+        isRetryEnabled = false;
+        LOGGER.info(StringConstants.FALLBACK_MESSAGE, exception);
+        Skill skillResponse = new Skill();
+        skillResponse.setMessage(StringConstants.FALLBACK_MESSAGE);
+        return ResponseEntity.status(HttpStatus.OK).body(skillResponse);
     }
 }
